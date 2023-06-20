cf607e0163bc135f55fa799e5b48dabbaf43c13e
imranBappy <imranbappy.official@gmail.com>
2023-06-20 16:38:01 +0600

    solve some responsive issue

--- client/src/features/products/productsApi.js
+++ client/src/features/products/productsApi.js
@@ -5,26 +5,6 @@ export const productsApi = apiSlice.injectEndpoints({
         getProducts: builder.query({
             query: ({ skip, limit }) => `/products?skip=${skip}&limit=${limit}`,
         }),
-        // for more products
-        getMoreProducts: builder.query({
-            query: ({ skip, limit }) => `/products?skip=${skip}&_limit=${limit}`,
-            async onQueryStarted({ limit }, { dispatch, queryFulfilled }) {
-                try {
-                    const data = await queryFulfilled;
-                    if (data?.data?.products?.length > 0) {
-                        console.log(`${limit}`);
-                        dispatch(apiSlice.util.updateQueryData("getProducts", `${limit}`, (draft) => {
-                            console.log(16, JSON.parse(JSON.stringify(draft.data)))
-                            console.log("data", data?.data?.products)
-                            // draft.data.
-                            // draft.data.push(...data?.products);
-                        }))
-                    }
-                } catch (error) {
-                    console.log(error);
-                }
-            },
-        }),
     })
 })
 
